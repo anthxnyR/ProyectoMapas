0a578f4ba64face53e04a345a4ad0cc0c14db929
Anthony Barrios <arbarrios.19@est.ucab.edu.ve>
2020-07-18 19:05:04 -0400

    Estructura Definida
    Se encuentra en Listas.h

---
+++ ProyectoMapas/Listas.h
@@ -0,0 +1,24 @@
+#ifndef LISTAS_H_INCLUDED
+#define LISTAS_H_INCLUDED
+
+typedef struct Lugares{
+    char Name[100];
+    int num;
+    struct Lugares *Next;
+    struct Camino *Ady;
+}
+
+typedef struct Camino{
+    char Name[100];
+    float Peso[3];
+    int num;
+    struct Rutas *Next;
+    struct Lugares *Ady, *Og;
+}
+
+struct Route{
+    struct Camino *path;
+    struct Route *next;
+}
+
+#endif // LISTAS_H_INCLUDED
