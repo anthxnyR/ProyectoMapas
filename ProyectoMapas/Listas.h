d0b7982591c662883cd5a16a209d40b94584c2cc
Anthony Barrios <arbarrios.19@est.ucab.edu.ve>
2020-07-19 01:52:46 -0400

    Funciones para crear los nodos lugares y camino
    También se creó la función para quitar espacios en blanco, facilitando la lectura.
    Se tiene que buscar la forma de leer las rutas y que sean almacenadas

--- ProyectoMapas/Listas.h
+++ ProyectoMapas/Listas.h
@@ -3,22 +3,59 @@
 
 typedef struct Lugares{
     char Name[100];
-    int num;
     struct Lugares *Next;
     struct Camino *Ady;
-}
+}Lugares;
 
 typedef struct Camino{
     char Name[100];
     float Peso[3];
-    int num;
     struct Camino *Next;
     struct Lugares *Ady, *Og;
-}
+}Camino;
 
 struct Route{
     struct Camino *path;
     struct Route *next;
+}Route;
+
+Lugares *LugarNuevo(char str[]){
+    Lugares *NewLugar=(Lugares *)malloc(sizeof(Lugares));
+    if(NewLugar==NULL){
+        printf("ERROR EN MALLOC\n");
+        EXIT_FAILURE;
+    }else{
+        strcpy(NewLugar->Name,str);
+        NewLugar->Next=NULL;
+        NewLugar->Ady=NULL;
+    }
+    return NewLugar;
+}
+
+Lugares *InsertLugar(Lugares *Map, Lugares *NewLugar){
+    Lugares *ptr;
+    if(Map==NULL) return NewLugar;
+    for(ptr=Map;ptr->Next;ptr=ptr->Next);
+    ptr->Next=NewLugar;
+    return Map;
+}
+
+Camino *CaminoNuevo(char str[],float P, float B, float C){
+    Camino *newCamino=(Camino *)malloc(sizeof(Camino));
+    if(newCamino==NULL){
+        printf("ERROR EN MALLOC\n");
+        EXIT_FAILURE;
+    }
+
+    else{
+        strcpy(newCamino->Name,str);
+        newCamino->Peso[0]=P;
+        newCamino->Peso[1]=B;
+        newCamino->Peso[2]=C;
+        newCamino->Ady=NULL.
+        newCamino->Next->NULL;
+    }
+    return newCamino;
 }
 
 #endif // LISTAS_H_INCLUDED
--- ProyectoMapas/main.c
+++ ProyectoMapas/main.c
@@ -1,3 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "Listas.h"
 
+void RemoveBlanks(char *str){
+    char straux[strlen(str)];
+    int i=0,j=0;
+    while(str[i]!='\0'){
+        if(!str[i]==' '){
+            straux[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    straux[j]='\0';
+    strcpy(str,straux);
+}
+
+int ValidarTxt(char namefile[]){ //no se si esta parte sea la mejor forma de leer
+    FILE *archivo=NULL;
+    char str[1000];
+    char array_str[1000]={" "};
+    archivo=fopen(namefile,"r");
+    Lugares *places=NULL;
+    Camino *camino=NULL;
+
+
+    while(!feof((archivo))){
+        fgets(str,100,archivo);
+        RemoveBlanks(str);
+        if(strcmp(str,"Lugares")){
+            fgets(str,100,archivo);
+            while(str!="Rutas"||!feof(archivo)){
+                RemoveBlanks(str);
+                places=InsertLugar(places,LugarNuevo(str));
+                fgets(str,100,archivo);
+            }
+            if(strcmp(str,"Rutas")){
+                fgets(str,100,archivo);
+                while(!feof(archivo)){
+                    RemoveBlanks(str);
+                }
+            }
+        }
+
+    }
+}
+
+int main(){
+    char namefile[100];
+
+
+    printf("Ingrese el nombre del archivo\n");
+    scanf("%s",namefile);
+    if(!strstr(namefile,".txt"))
+        strcat(path,".txt");
+    FILE *fp=NULL;
+    fp=fopen(namefile,"r");
+    if(fp==NULL){
+        printf("ARCHIVO NO ENCONTRADO!\n");
+        return 0;
+    }else fclose(fp);
+
+    ValidarTxt(namefile);
+
+}
